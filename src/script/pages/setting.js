d04525425798b20ca22eaae047a5319d909c4b13
Xing <xing@xxx.com>
2026-04-11 10:08:08 +0800

    color

--- src/script/pages/setting.js
+++ src/script/pages/setting.js
@@ -7,9 +7,9 @@ export function loadSetting() {
 function constructSettingPage() {
     const pageNode = document.querySelector(".setting-page");
     pageNode.appendChild(constructSection("显示", [
-        constructButton("主题色", "", () => constructDisplayPage()),
-        constructButton("安全区", ""),
-        constructButton("详细设置", ""),
+        constructButton("主题色", "", () => constructDisplaySetting()),
+        constructButton("安全区", "", () => constructDisplaySetting()),
+        constructButton("详细设置", "", () => constructDisplaySetting()),
     ]));
     pageNode.appendChild(constructSection("存储", [
         constructButton("启用缓存", ""),
@@ -25,14 +25,46 @@ function constructSettingPage() {
     ]));
 }
 
-function constructDisplayPage() {
-    pushNewPage(displayPageConstructor, "#navigation-setting");
+function constructDisplaySetting() {
+    pushNewPage(displaySettingConstructor, "#navigation-setting");
 }
 
-function displayPageConstructor() {
+function displaySettingConstructor() {
+    const displayPageTrackNode = document.createElement("div");
+    displayPageTrackNode.setAttribute("class", "carousel-track");
+    displayPageTrackNode.setAttribute("id", "dispaly-setting-track")
+
     const displayPageNode = document.createElement("div");
-    displayPageNode.setAttribute("class", "display-page");
-    return displayPageNode;
+    displayPageNode.setAttribute("class", "setting-page");
+
+    function setThemeColor(color) {
+        document.documentElement.style.setProperty("--theme-color", color);
+    }
+
+    displayPageNode.appendChild(constructSection("夜间模式", [
+        constructButton("跟随系统", "", ),
+        constructButton("明亮", "", ),
+        constructButton("暗色", "", ),
+    ]))
+
+    displayPageNode.appendChild(constructSection("主题色", [
+        constructButton("陶瓷蓝", "", () => setThemeColor("#1e59a8")),
+        constructButton("哔哩粉", "", () => setThemeColor("#fb7299")),
+        constructButton("书小红", "", () => setThemeColor("#ff2e4d")),
+        constructButton("巨信绿", "", () => setThemeColor("#1aad19")),
+        constructButton("清华紫", "", () => setThemeColor("#6703b1")),
+        constructButton("自定义", "", ),
+    ]));
+
+    displayPageNode.appendChild(constructSection("安全区", [
+        constructButton("顶部", "", ),
+        constructButton("底部", "", ),
+        constructButton("左侧", "", ),
+        constructButton("右侧", "", ),
+    ]));
+
+    displayPageTrackNode.appendChild(displayPageNode);
+    return displayPageTrackNode;
 }
 
 function constructSection(sectionTitle, items) {
